fe813f7708df4ae80ac651dbd22a9e5f4d48bec7
Quentin McKay <quentinmckay1@gmail.com>
2022-12-13 16:16:36 +1100

    added president or pm scrape

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ os.system('clear')
 # country = input('Country: ')
 
 country = 'Argentina'
-country = 'Morocco'
+country = 'Japan'
 response = requests.get(f'https://en.wikipedia.org/wiki/{country}')
 
 
@@ -20,7 +20,7 @@ p_tags = soup.select('.mw-parser-output > p') # hook in and get list of <p>'s
 
 info_paragraph = p_tags[1].get_text() # get text from 1st <p>
 
-def filter_country_name(string, country_name):
+def filter_country_name(string, country_name, replacement=''):
 	start_name = country_name[:4] # slice the first 4 letters
 	end_name = country_name[-4:] # slice the last 4 letters 
 
@@ -29,7 +29,7 @@ def filter_country_name(string, country_name):
 	# note: uses a raw f-string and the re.I makes it case-insensitive
 	regex = re.compile(rf'\w*({start_name}|{end_name})\w*', re.I) 
 
-	return regex.sub('_', string) # substitute _ wherever the word would have appeared
+	return regex.sub(replacement, string) # substitute _ wherever the word would have appeared
 
 
 def remove_footnotes_and_parens(string):
@@ -39,10 +39,10 @@ def remove_footnotes_and_parens(string):
 	return regex.sub('', string)
 
 
-info_paragraph = filter_country_name(info_paragraph, country) # filter the 
+info_paragraph = filter_country_name(info_paragraph, country, replacement='_') # filter the 
 
-print(info_paragraph)
-print(remove_footnotes_and_parens((info_paragraph)))
+# print(info_paragraph)
+# print(remove_footnotes_and_parens((info_paragraph)))
 # print(info_paragraph.split('.')[0]) 
 # print(choice(info_paragraph.split('.')))
 
@@ -53,22 +53,36 @@ th_tags = soup.select('.ib-country')[0].find_all('th') # get list <th>'s
 # print(th_tags)
 
 info = {}
-for th_tag in th_tags:
-    strings = list(th_tag.strings)
-    if strings and strings[0] == 'Capital':
-        td = th_tag.find_next_sibling('td')
-        # get string of first <a> in the <td>
-        capital = td.find('a').string
-        info['capital'] = capital
-    if strings and strings[0] == 'Currency':
-        td = th_tag.find_next_sibling('td')
-        # get string of first <a> in the <td>
-        currency = td.find('a').string
-        info['currency'] = currency
 
+for th_tag in th_tags:
+
+	strings = list(th_tag.strings)
+	print(strings)
+
+	if strings:
+
+		if strings[0] == 'Capital':
+			td = th_tag.find_next_sibling('td') # find corresponding <td>
+			capital = td.find('a').string # get string of first <a> in the <td>
+			info['capital'] = capital
+
+		if strings[0] == 'Currency':
+			td = th_tag.find_next_sibling('td') # find corresponding <td>
+			currency = td.find('a').string # get string of first <a> in the <td>
+			currency = filter_country_name(currency, country, replacement='').strip()
+			info['currency'] = currency
+
+		if 'President' in strings:
+			td = th_tag.find_next_sibling('td') # find corresponding <td>
+			president = td.find('a').string # get string of first <a> in the <td>
+			info['president'] = president
+		elif 'Prime Minister' in strings:
+			td = th_tag.find_next_sibling('td') # find corresponding <td>
+			prime_minister = td.find('a').string # get string of first <a> in the <td>
+			info['prime minister'] = prime_minister
 print()
 print(info)
-        
+		
 
 # tr_tags = soup('div', string="and largest city")[0].parent.strings
 # print(list(tr_tags))
