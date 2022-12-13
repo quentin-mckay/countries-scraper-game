84d9253b92984111c327569b79d3c947d2e19829
Quentin McKay <quentinmckay1@gmail.com>
2022-12-13 11:17:13 +1100

    added regex filters

--- app.py
+++ app.py
@@ -1,20 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+import os
+from random import choice
 
+os.system('clear')
 
-country = input('Country: ')
+# country = input('Country: ')
+
+country = 'Argentina'
+country = 'Morocco'
 response = requests.get(f'https://en.wikipedia.org/wiki/{country}')
 
-# response = requests.get(f'https://en.wikipedia.org/wiki/Argentina')
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# ==== working ====
-p_tags = soup.select('.mw-parser-output > p')
-info_paragraph = p_tags[1].get_text()
-# print(info_paragraph)
+# ===== get info paragraph =====
+p_tags = soup.select('.mw-parser-output > p') # hook in and get list of <p>'s
 
-th_tags = soup.select('.ib-country')[0].find_all('th')
+info_paragraph = p_tags[1].get_text() # get text from 1st <p>
+
+def filter_country_name(string, country_name):
+	start_name = country_name[:4] # slice the first 4 letters
+	end_name = country_name[-4:] # slice the last 4 letters 
+
+	# regex test for words that include either first or last part of country name
+	# preceded or followed by any number of letters
+	# note: uses a raw f-string and the re.I makes it case-insensitive
+	regex = re.compile(rf'\w*({start_name}|{end_name})\w*', re.I) 
+
+	return regex.sub('_', string) # substitute _ wherever the word would have appeared
+
+
+def remove_footnotes_and_parens(string):
+	# match either (.....) or [.....]
+	# note: uses non-greedy capture modifier and
+	regex = re.compile(r'\[.*?\]|\(.*?\)\)*')
+	return regex.sub('', string)
+
+
+info_paragraph = filter_country_name(info_paragraph, country) # filter the 
+
+print(info_paragraph)
+print(remove_footnotes_and_parens((info_paragraph)))
+# print(info_paragraph.split('.')[0]) 
+# print(choice(info_paragraph.split('.')))
+
+
+# ===== get table data =====
+
+th_tags = soup.select('.ib-country')[0].find_all('th') # get list <th>'s
 # print(th_tags)
 
 info = {}
@@ -31,6 +66,7 @@ for th_tag in th_tags:
         currency = td.find('a').string
         info['currency'] = currency
 
+print()
 print(info)
         
 
