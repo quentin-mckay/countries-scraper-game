01c6abf697f7813ec59587f13d61e04066c23b8a
Quentin McKay <quentinmckay1@gmail.com>
2022-12-14 11:56:45 +1100

    added random sentence hint

--- app.py
+++ app.py
@@ -6,22 +6,27 @@ from random import choice, randint
 from countries import countries # list of 32 countries
 from colr import color
 
-def random_color():
-	return (randint(0, 256), randint(0, 256), randint(0, 256))
 
 
-text = color('hello', fore=random_color())
-# text2 = color('hello', fore=(255, 128, 0))
+# def random_color():
+# 	return (randint(0, 256), randint(0, 256), randint(0, 256))
 
-sentence = "This is a random sentence of words"
 
-sentence = ' '.join([color(word, fore=random_color()) for word in sentence.split()])
+# text = color('hello', fore=random_color())
+# # text2 = color('hello', fore=(255, 128, 0))
 
-print(text)
+# sentence = "This is a random sentence of words"
 
-print(sentence)
+# sentence = ' '.join([color(word, fore=random_color()) for word in sentence.split()])
 
-# os.system('clear')
+# print(text)
+
+# print(sentence)
+
+
+
+
+os.system('clear')
 
 # print(countries)
 
@@ -35,17 +40,7 @@ country = 'Denmark'
 # country = 'Wales'
 # country = 'South_Korea'
 
-country = choice(countries)
 
-print()
-print(country)
-
-response = requests.get(f'https://en.wikipedia.org/wiki/{country}')
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# ===== get info paragraph =====
-p_tags = soup.select('.mw-parser-output > p') # hook in and get list of <p>'s
-info_paragraph = p_tags[1].get_text() # get text from 1st <p>
 
 
 # ============================== regex cleaning functions ==============================
@@ -71,101 +66,139 @@ def remove_footnotes_and_parens(string):
 	return regex.sub('', string)
 
 
-info_paragraph = filter_country_name(info_paragraph, country, replacement='_') # filter the 
+def clean_text(text, country):
+	text = filter_country_name(text, country, replacement='_')
+	text = remove_footnotes_and_parens(text)
+	return text
 
 # print(info_paragraph)
 # print(remove_footnotes_and_parens((info_paragraph)))
 # print(info_paragraph.split('.')[0]) 
 # print(choice(info_paragraph.split('.')))
-
-
-# ============================== Fill info dictionary ========================================
-info = {}
-
-
-# ===== get anthem =====
-anthem = ''
-try:
-	td_tag = soup.select('.anthem')[0]
-	strings_list = list(td_tag.stripped_strings)
-	last_few = strings_list[-3:] # anthem is always in one of the last few spots
-	anthem = [s for s in last_few if len(s) > 5][-1] # find the one that's not a punctuation
-
-	# print()
-	# print(list(strings_list))
-	print()
-	print(anthem.replace('"', ''))
-except IndexError: # for some reason Denmark is different to every other country
-	print('no anthem')
-
-
-info['anthem'] = anthem.replace('"', '') # get rid of quotes if needed
-
-
-
-# ===== get table data =====
-th_tags = soup.select('.vcard')[0].find_all('th') # get list <th>'s
-# print(th_tags)
-
-
-for th_tag in th_tags:
-
-	strings = list(th_tag.strings)
-	# print(strings)
-
-	if strings:
-
-		if strings[0] == 'Capital':
-			td = th_tag.find_next_sibling('td') # find corresponding <td>
-			capital = td.find('a').string # get string of first <a> in the <td>
-			info['capital'] = capital
-
-		if strings[0] == 'Currency':
-			td = th_tag.find_next_sibling('td') # find corresponding <td>
-			currency = td.find('a').string # get string of first <a> in the <td>
-			currency = filter_country_name(currency, country, replacement='').strip()
-			info['currency'] = currency
-
-		# check for either President or Prime Minister
-		if 'President' in strings:
-			td = th_tag.find_next_sibling('td') # find corresponding <td>
-			president = td.find('a').string # get string of first <a> in the <td>
-			info['president'] = president
-		elif 'Prime Minister' in strings:
-			td = th_tag.find_next_sibling('td') # find corresponding <td>
-			prime_minister = td.find('a').string # get string of first <a> in the <td>
-			info['prime minister'] = prime_minister
-
-print()
-print(info, '\n')
 		
 
+def scrape_country_info(country):
+	info = {}
+
+	response = requests.get(f'https://en.wikipedia.org/wiki/{country}')
+	soup = BeautifulSoup(response.text, 'html.parser')
+
+	# ===== get info paragraph =====
+	p_tags = soup.select('.mw-parser-output > p') # hook in and get list of <p>'s
+
+	paragraph = p_tags[1].get_text() # get text from 1st <p>
+	info['first paragraph'] = clean_text(paragraph, country)
+
+	paragraph = p_tags[2].get_text() # get text from 1st <p>
+	info['second paragraph'] = clean_text(paragraph, country)
 
 
+
+	# ============================== Fill info dictionary ========================================
+
+
+	# ===== get anthem =====
+	anthem = ''
+	try:
+		td_tag = soup.select('.anthem')[0]
+		strings_list = list(td_tag.stripped_strings)
+		last_few = strings_list[-3:] # anthem is always in one of the last few spots
+		anthem = [s for s in last_few if len(s) > 5][-1] # find the one that's not a punctuation
+
+		# print()
+		# print(list(strings_list))
+	except IndexError: # for some reason Denmark is different to every other country
+		print('no anthem')
+
+
+	info['anthem'] = anthem.replace('"', '') # get rid of quotes if needed
+
+
+
+	# ===== get table data =====
+	th_tags = soup.select('.vcard')[0].find_all('th') # get list <th>'s
+	# print(th_tags)
+
+
+	for th_tag in th_tags:
+
+		strings = list(th_tag.strings)
+		# print(strings)
+
+		if strings:
+
+			if strings[0] == 'Capital':
+				td = th_tag.find_next_sibling('td') # find corresponding <td>
+				capital = td.find('a').string # get string of first <a> in the <td>
+				info['capital'] = capital
+
+			if strings[0] == 'Currency':
+				td = th_tag.find_next_sibling('td') # find corresponding <td>
+				currency = td.find('a').string # get string of first <a> in the <td>
+				currency = filter_country_name(currency, country, replacement='').strip()
+				info['currency'] = currency
+
+			# check for either President or Prime Minister
+			if 'President' in strings:
+				td = th_tag.find_next_sibling('td') # find corresponding <td>
+				president = td.find('a').string # get string of first <a> in the <td>
+				info['president'] = president
+			elif 'Prime Minister' in strings:
+				td = th_tag.find_next_sibling('td') # find corresponding <td>
+				prime_minister = td.find('a').string # get string of first <a> in the <td>
+				info['prime minister'] = prime_minister
+
+	return info
 
 def play_game():
 	answer_country = choice(countries) # pick random country
 
+	print()
+	print(answer_country, '\n')
+
 	country_info = scrape_country_info(answer_country)
+	# print(country_info)
 
-	guesses_remaining = 6
+	starting_guesses = 6
+	guesses_remaining = starting_guesses
 
-	print(answer_country)
 
 	while guesses_remaining > 0:
 
 		# display options
 		guess_word = 'guesses' if guesses_remaining > 0 else 'guess'
-		print(f"{guesses_remaining} {guess_word} remaining. Select a hint option: ")
+		print(f"{guesses_remaining} {guess_word} remaining. Select a hint option: \n")
 		options = [
 				'[1] Random sentence',
 				'[2] Random fact',
-				'[3] Flag colour text'
+				'[3] Flag colour text',
+				''
 			]
 		[print(option) for option in options]
 
+
+		hint_option = ''
+		while hint_option not in ('1', '2', '3'):
+			hint_option = input('> ')
+			print()
+		
+
+		match hint_option:
+			# Random Sentence
+			case '1':
+				sentences = country_info['second paragraph'].split('.')
+				# print(sentences)
+
+				sentence = ''
+				while len(sentence) < 5: # filter out occasional oddities
+					sentence = choice(sentences)
+
+				print("Here's a random sentence: \n")
+				print(sentence + '.', '\n')
+
 		# get guess from user
 		guess = input("Guess the country: ")
+		print()
 
 		if guess == answer_country:
 			print("You win!")
@@ -184,10 +217,10 @@ def show_high_scores():
 
 def start():
 
-	print('--- Welcome to the World Cup Country Quiz Game ---\n')
-	print('Please select an option:')
+	print('\n--- Welcome to the World Cup Country Quiz Game ---\n')
+	print('Please select an option:\n')
 
-	options = ['[1] Play quiz game', '[2] Show High Scores']
+	options = ['[1] Play quiz game', '[2] Show High Scores', '']
 	[print(option) for option in options] # print out each option on a new line
 
 	menu_choice = input('> ')
